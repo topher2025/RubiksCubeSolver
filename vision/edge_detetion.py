3fa92bc2ff9d21288cd2c11f15b66e4ff029c540
Christopher Lewis <topher.the.good.ninja@gmail.com>
2026-06-09 19:58:06 -0500

    Finished laplacian

--- vision/edge_detetion.py
+++ vision/edge_detetion.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from time import sleep
 
-from . import util
+import util
 
 
 DEBUG = True
@@ -11,16 +11,18 @@ DEBUG = True
 def laplacian(path=None, pimg=None, rimg=None):
     global DEBUG
     image = util.args(path, pimg, rimg)
-    if not image:
+    if image is None:
         return None
     
     lap = cv2.Laplacian(image, cv2.CV_64F)
     lap = cv2.convertScaleAbs(lap)
 
     if DEBUG:
+        print("[DEBUG] Original")
         cv2.imshow("Original", image)
+        print("[DEBUG] Laplacian")
         cv2.imshow("Laplacian", lap)
-        sleep(5)
+        cv2.waitKey(0)
     
     return lap
 
@@ -28,7 +30,7 @@ def laplacian(path=None, pimg=None, rimg=None):
 def sobel(path=None, pimg=None, rimg=None):
     global DEBUG
     image = util.args(path, pimg, rimg)
-    if not image:
+    if image is None:
         return None
     
     sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0)
@@ -37,12 +39,20 @@ def sobel(path=None, pimg=None, rimg=None):
     sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
     if DEBUG:
+        print("[DEBUG] Original")
+        cv2.imshow("Original", image)
+        print("[DEBUG] Soble X")
         cv2.imshow("Sobel X", sobelX)
+        print("[DEBUG] Soble Y")
         cv2.imshow("Sobel Y", sobelY)
+        print("[DEBUG] Soble Combined")
         cv2.imshow("Sobel Combined", sobelCombined)
-        sleep(5)
 
 
 if "__main__" == __name__:
-    pass
+    if DEBUG:
+        print("[DEBUG] Debug active")
+    p='/home/to/PycharmProjects/RubiksCubeSolver/tests/images/front_solved_min.jpeg'
+    
+    laplacian(path=p)
 
